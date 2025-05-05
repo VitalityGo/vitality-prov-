665062b0f750c9b4f9607856bbd7a4d693d5eca0
vitalitygo5 <vitalitygo5@gmail.com>
2025-05-05 19:48:37 +0000

    config commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'VitalityGo.app',
   appName: 'Vitality Go ',
-  webDir: 'dist'
+  webDir: 'dist/myapp/browser'
 };
 
 export default config;
