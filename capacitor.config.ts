94ea20b40af6bc810558f46f6825fae31cbbcb23
vitalitygo5 <vitalitygo5@gmail.com>
2025-05-07 16:28:02 +0000

    logofinal

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -11,8 +11,14 @@ const config: CapacitorConfig = {
   plugins: {
     GoogleAuth: {
       scopes: ['profile', 'email'],
-      serverClientId: '1023939118177-12t6esvjfvvanihmp05jjdqbbt3b49da.apps.googleusercontent.com', // Reemplaza con tu Client ID
+      serverClientId: '1023939118177-12t6esvjfvvanihmp05jjdqbbt3b49da.apps.googleusercontent.com',
       forceCodeForRefreshToken: true
+    },
+    CapacitorAssets: {
+      iconPath: 'resources/icon.png',
+      splashPath: 'resources/splash.png',
+      androidIconDensities: ['mdpi', 'hdpi', 'xhdpi', 'xxhdpi', 'xxxhdpi'],
+      androidSplashDensities: ['mdpi', 'hdpi', 'xhdpi', 'xxhdpi', 'xxxhdpi']
     }
   }
 };
